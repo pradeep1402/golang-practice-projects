3d575411653652753a68c30a77b8bcd21be1fbbe
Pradeep <107491133+pradeep1402@users.noreply.github.com>
2025-07-07 11:00:14 +0530

    console greeting is done

--- console-greeting/main.go
+++ console-greeting/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go [name]")
+		return
+	}
+
+	fmt.Printf("Hello, %s! Welcome to Go", os.Args[1])
 }
