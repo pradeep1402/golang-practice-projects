9e1719553274e176cc2038b556160d102078f1db
Pradeep <107491133+pradeep1402@users.noreply.github.com>
2025-07-17 21:45:24 +0530

    create script files to genrate auth go code

---
+++ shared-proto/scripts/auth.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+protoc \
+  --proto_path=auth \
+  --go_out=gen/auth \
+  --go-grpc_out=gen/auth \
+  --go_opt=paths=source_relative \
+  --go-grpc_opt=paths=source_relative \
+  auth/auth.proto
